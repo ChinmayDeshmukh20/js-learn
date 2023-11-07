a945f40a3d7a6384253d1300fb0bda2545d82aea
ChinmayDeshmukh20 <73434511+ChinmayDeshmukh20@users.noreply.github.com>
2023-11-07 16:06:50 +0000

    comparsion imp things

---
+++ 01_basics/04_comparsion.js
@@ -0,0 +1,18 @@
+console.log("hello")
+console.log("2">1);
+console.log("02">1);
+console.log("01">1);
+
+// this kind of comparsion should be avoided , as output cant be predicated always correctly
+// == works fine if both datatypes are same , and if both datatypes are different as in below examples , then it creates an issue
+// == converts null into 0 , while for <= , >= it won't be converted that way ,  so avoid using such bad ways of coding
+console.log(null==0);
+console.log(null>=0);
+console.log(null>0);
+
+
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log(undefined>0);
+
+// in JS == (equality) and > , < , <= , >= (comparators) both work differently
