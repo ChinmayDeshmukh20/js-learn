6e342d7f67721fa10b98d3c710dc262e1ce6b413
ChinmayDeshmukh20 <73434511+ChinmayDeshmukh20@users.noreply.github.com>
2023-11-07 16:31:11 +0000

    ksjd bvjksjsdlfnds

--- 01_basics/04_comparsion.js
+++ 01_basics/04_comparsion.js
@@ -6,6 +6,9 @@ console.log("01">1);
 // this kind of comparsion should be avoided , as output cant be predicated always correctly
 // == works fine if both datatypes are same , and if both datatypes are different as in below examples , then it creates an issue
 // == converts null into 0 , while for <= , >= it won't be converted that way ,  so avoid using such bad ways of coding
+
+// use === for strict checking , === will check if both datatypes are same and then only it will proceed further
+
 console.log(null==0);
 console.log(null>=0);
 console.log(null>0);
