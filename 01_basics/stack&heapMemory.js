df2fad33aa5fa9795ebd22b64be20af8b330bbb7
ChinmayDeshmukh20 <73434511+ChinmayDeshmukh20@users.noreply.github.com>
2023-11-08 13:41:37 +0000

    notes on memory

---
+++ 01_basics/stack&heapMemory.js
@@ -0,0 +1,7 @@
+//prmitive datatypes -> String , Number , Boolean , Null , undefined , Symbol , BigInt
+//Non-primitive datatypes(aka Reference types) -> Array , Objects, Functions
+
+//Types of memory - stack & heap memory
+// Primitive types are stored in Stack && non-primitives in Heap
+// Whenever we create anything in Stack memory we get copy of that to play with && 
+//whenever we create in Heap memory we get refernce of it , so whatever we do , it directly gets changed in original declarations
