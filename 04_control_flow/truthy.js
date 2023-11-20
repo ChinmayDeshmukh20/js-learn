39310e6db1decdd0d2ec7c20ceebb08b588b0eea
ChinmayDeshmukh20 <73434511+ChinmayDeshmukh20@users.noreply.github.com>
2023-11-20 09:59:27 +0000

    if else , truthy , switch , ?? , ternitary

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,44 @@
+const userEmail = []
+
+if (userEmail) {                // here we are not checking is userEmail equal to anything , so by default we are considering it true, that is called truthy value
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {                   // CHECKING IF OBJECT IS EMPTY // Object.keys(value) will return us an array & then we are checking the length
+    console.log("Object is empty");
+}
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15        // basically here we want to avoid having null as output , so we excute the OR statement in its place , or in real world we write a function
+val1 = null ?? 10 ?? 20
+
+
+
+console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
