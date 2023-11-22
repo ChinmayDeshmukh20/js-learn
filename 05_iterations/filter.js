fb84271eddc8ae32ddc45861b93fb2da8498f3df
ChinmayDeshmukh20 <73434511+ChinmayDeshmukh20@users.noreply.github.com>
2023-11-22 10:48:50 +0000

    looopssss

---
+++ 05_iterations/filter.js
@@ -0,0 +1,57 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+
+// const values = coding.forEach( (item) => {
+//     //console.log(item);
+//     return item
+// } )
+
+// console.log(values);
+
+
+
+
+
+// FOR EACH CAN ALSO DO THE SAME THING AS FILTER , BUT IT CAN'T RETURN SIMPLE ENTITY , INCASE WE NEED TO DO FURTHER OPERATIONS ON IT
+
+
+
+
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter( (num) => {
+//     return num > 4
+// } )
+
+// const newNums = []
+
+// myNums.forEach( (num) => {                // SAME THING IS DONE USING FOR EACH
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// } )
+
+// console.log(newNums);
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  let userBooks = books.filter( (bk) => bk.genre === 'History')        // (() =>)     //  when we write arrow functions like this , we are implicity(it is understood that it is return , no need to write return keyword) returning the result
+
+  let userBooks1 = books.filter( (bk) => { 
+    return bk.publish >= 1995 && bk.genre === "History"          // (() => {})   //  when we write things in scope {} , then we explicity need to write return keyword in return statement
+})
+  console.log(userBooks);
+  console.log(userBooks1);
---
+++ 05_iterations/forEach.js
@@ -0,0 +1,47 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// WE PASS CALLBACK(FUNCTION WITHOUT NAME) FUNCTION IN FOR EACH LOOP
+
+
+
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=> {                 // for each loop in each iteration also holds up index and the whole array
+//     console.log(item, index, arr);
+// } )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )
+
+
+//  FOR EACH LOOP CAN ITERATE OBJECTS
+
